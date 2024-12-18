571d422b4db132151ff032c41dee74c99a174295
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2024-12-18 13:33:19 -0300

    first commit

---
+++ src/test/kotlin/br/com/example/rest_with_kotlin/RestWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.example.rest_with_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
